20a25fa17e2d245a478e119ce417f7802b88924a
LiangYuJUI <liangyujui@gmail.com>
2024-06-21 00:43:13 +0800

    finish app/annountment & app/activity

--- frontend/app/announcement/page.tsx
+++ frontend/app/announcement/page.tsx
@@ -3,6 +3,7 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 import { useState, useEffect } from 'react'
 import { Sidebar } from '../sidebar/page'
+import axios from 'axios'
 
 interface Announcement {
   id: string,
@@ -11,88 +12,96 @@ interface Announcement {
   time: string,
 }
 
-const Announcements: Announcement[] = [
-  {
-    id: "1",
-    title: 'New Yoga Class',
-    content: 'We are excited to announce a new Yoga class starting next Monday.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "2",
-    title: 'Holiday Schedule',
-    content: 'The gym will be closed on July 4th for Independence Day.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "3",
-    title: 'New Equipment',
-    content: 'We have added new treadmills and ellipticals to the cardio room.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "4",
-    title: 'Member of the Month',
-    content: 'Congratulations to Jane Doe for being the member of the month!',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "5",
-    title: 'Workshop: Nutrition Basics',
-    content: 'Join us for a workshop on nutrition basics next Saturday.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "6",
-    title: 'Extended Hours',
-    content: 'We are extending our hours on weekdays until 10 PM.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "7",
-    title: 'Personal Training Discount',
-    content: 'Get 20% off personal training sessions this month.',
-    time: '2024-06-15T00:00:00Z'
-  },
-  {
-    id: "8",
-    title: 'Upcoming Maintenance',
-    content: 'The pool will be closed for maintenance next Tuesday.',
-    time: '2024-06-15T00:00:00Z'
-  }
-];
+// const Announcements: Announcement[] = [
+//   {
+//     id: "1",
+//     title: 'New Yoga Class',
+//     content: 'We are excited to announce a new Yoga class starting next Monday.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "2",
+//     title: 'Holiday Schedule',
+//     content: 'The gym will be closed on July 4th for Independence Day.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "3",
+//     title: 'New Equipment',
+//     content: 'We have added new treadmills and ellipticals to the cardio room.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "4",
+//     title: 'Member of the Month',
+//     content: 'Congratulations to Jane Doe for being the member of the month!',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "5",
+//     title: 'Workshop: Nutrition Basics',
+//     content: 'Join us for a workshop on nutrition basics next Saturday.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "6",
+//     title: 'Extended Hours',
+//     content: 'We are extending our hours on weekdays until 10 PM.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "7",
+//     title: 'Personal Training Discount',
+//     content: 'Get 20% off personal training sessions this month.',
+//     time: '2024-06-15T00:00:00Z'
+//   },
+//   {
+//     id: "8",
+//     title: 'Upcoming Maintenance',
+//     content: 'The pool will be closed for maintenance next Tuesday.',
+//     time: '2024-06-15T00:00:00Z'
+//   }
+// ];
 
 export default function ActivityPage() {
+  const [Announcements, setAnnouncements] = useState<Announcement[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  // const [announcements, setAnnouncements] = useState<Announcement[]>([]);
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const jwtToken = localStorage.getItem('jwtToken') || '';
+    console.log(jwtToken)
+    const fetchAnnouncements = async () => {
+      try {
+        const headers = {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${jwtToken}`,
+        };
 
-  // useEffect(() => {
-  //     const fetchAnnouncements = async () => {
-  //         try {
-  //             const response = await fetch('/api/announcements');
-  //             if (!response.ok) {
-  //                 throw new Error('Network response was not ok');
-  //             }
-  //             const data = await response.json();
-  //             setAnnouncements(data);
-  //         } catch (error: unknown) {
-  //             if (error instanceof Error) {
-  //                 setError(error.message);
-  //             } else {
-  //                 setError('An unknown error occurred');
-  //             }
-  //         } finally {
-  //             setLoading(false);
-  //         }
-  //     };
+        const start = '1989-06-01';
+        const endDate = new Date();
+        const end = endDate.toISOString().split('T')[0];
+        const params = {
+          start: start,
+          end: end
+        };
 
-  //     fetchAnnouncements();
-  // }, []);
+        const res = await axios.get('http://localhost:8080/announcements');
+        setAnnouncements(res.data);
+        setLoading(false);
+        console.log('Response:', res.data);
+      } catch (error) {
+        console.error('Error:', error);
+        setError('Failed to fetch activities');
+        setLoading(false);
+      }
+    };
 
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error}</div>;
+    fetchAnnouncements();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
 
   return (
